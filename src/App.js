6415405c87e518d6bcda7c8ce16564ea3f002e31
bendamian <badamian@gmail.com>
2023-01-04 18:28:26 +0000

    adding a form and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 
-
+import { useState,useEffect } from "react";
+import Form from "./Form";
 
 function App() {
+const API_URL = "https://jsonplaceholder.typicode.com/";
+const [reqType,setReqType] =useState('users')
+const [item,setItem] = useState([])
+
+useEffect(()=> {
+const fetchItems = async () => {
+try {
+  const response = await fetch(`${API_URL}${reqType}`)
+  const data = await response.json()
+  setItem(data)
+} catch (err) {
+  console.log(err)
+}
+
+}
+fetchItems()
+},[reqType])
+
+
+
+
   return (
     <div className="App">
-      
+      <Form  reqType={reqType} setReqType={setReqType} />
     </div>
   );
 }
